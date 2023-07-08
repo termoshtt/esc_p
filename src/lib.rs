4dcc2271550ed9187fead47c878f998e1f4865f8
Toshiki Teramura <toshiki.teramura@gmail.com>
2023-07-08 22:14:19 +0900

    Trying

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,20 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+//! Tiny crate for drawing pictures on terminal by [Sixel graphics](https://www.vt100.net/docs/vt3xx-gp/chapter14.html)
+
+pub const DSC: u8 = 0x90;
+pub const ST: u8 = 0x9C;
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::io::Write;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn print() {
+        let ctl = vec![
+            DSC, ';' as u8, 0, /* P2 */
+            0, /* P3 */
+            'q' as u8, 1, 1, 1, ST,
+        ];
+        std::io::stdout().write_all(&ctl).unwrap();
     }
 }
